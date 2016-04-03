1a7d7ef9b1a1c288311a9dcb3e70dcfe29885339
danielcaldas <danielcaldas@sapo.pt>
2016-04-03 12:57:47 +0100

    Added basic template for print shop employee.

--- app/app.routes.js
+++ app/app.routes.js
@@ -8,7 +8,10 @@ angular.module("ProxyPrint").config(['$routeProvider', function ($routeProvider 
          templateUrl: '/app/components/home/views/register.html'
       }).
       when('/:userID', {
-         templateUrl: '/app/components/consumer/views/customer_mainpage.html'
+         templateUrl: '/app/components/consumer/views/consumer_mainpage.html'
+      }).
+      when('/pshop/employee/:userID', {
+        templateUrl: '/app/components/printshop/employee/views/pending.html'
       }).
       otherwise({
          redirectTo: '/'
