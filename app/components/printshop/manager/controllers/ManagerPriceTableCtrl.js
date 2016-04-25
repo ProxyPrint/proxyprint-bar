bc06cd343cd82ea5c0db122dc6de5f9a6ffece8e
danielcaldas <danielcaldas@sapo.pt>
2016-04-25 17:41:17 +0100

    Added some static objects to ManagerPriceTableCtrl.js.

--- app/components/printshop/manager/controllers/ManagerPriceTableCtrl.js
+++ app/components/printshop/manager/controllers/ManagerPriceTableCtrl.js
@@ -1,6 +1,19 @@
 angular.module('ProxyPrint').controller('ManagerPriceTableCtrl', ['$scope',
-      function($scope) {
+function($scope) {
 
-// Code
+  $scope.priceTable = {
+    "BW": [
+      {infLim: "1", supLim: "20", priceA4SIMPLEX: 0.10, priceA4DUPLEX: 0.12, priceA3SIMPLEX: 0.22, priceA3DUPLEX: 0.20},
+      {infLim: "21", supLim: "30", priceA4SIMPLEX: 0.10, priceA4DUPLEX: 0.12, priceA3SIMPLEX: 0.22, priceA3DUPLEX: 0.20},
+      {infLim: "31", supLim: "100", priceA4SIMPLEX: 0.10, priceA4DUPLEX: 0.12, priceA3SIMPLEX: 0.22, priceA3DUPLEX: 0.20}
+    ],
+    "COLOR": [
+      {infLim: "1", supLim: "20", priceA4SIMPLEX: 0.10, priceA4DUPLEX: 0.12, priceA3SIMPLEX: 0.22, priceA3DUPLEX: 0.20},
+      {infLim: "21", supLim: "30", priceA4SIMPLEX: 0.10, priceA4DUPLEX: 0.12, priceA3SIMPLEX: 0.22, priceA3DUPLEX: 0.20},
+      {infLim: "31", supLim: "100", priceA4SIMPLEX: 0.10, priceA4DUPLEX: 0.12, priceA3SIMPLEX: 0.22, priceA3DUPLEX: 0.20}
+    ]
+  };
+
+  $scope.isStaplingFree = false;
 
 }]);
