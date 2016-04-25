e6406cd78494f69bb12bd2807f3464319f74d0ae
danielcaldas <danielcaldas@sapo.pt>
2016-04-25 23:00:13 +0100

    Some realy goddess design was made.

--- app/components/printshop/manager/controllers/ManagerPriceTableCtrl.js
+++ app/components/printshop/manager/controllers/ManagerPriceTableCtrl.js
@@ -32,10 +32,46 @@ app.controller('ManagerPriceTableCtrl', ['$scope', '$uibModal', 'PriceTableServi
     });
   };
 
+  $scope.openNewEntryPrintCopyModal = function(reply) {
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: 'app/components/printshop/manager/views/new-printcopy-row-modal.html',
+      controller: 'NewPrintCopyEntryCtrl',
+      size: 'sm',
+      resolve: {
+        index: function() {
+          return $scope.index;
+        },
+        text: function() {
+          return reply;
+        }
+      }
+    });
+    modalInstance.result.then(function(index) {
+      // do stuff
+    });
+  };
+
 }]);
 
 // Modal for deleting a row of certain table from price table
 app.controller('ConfirmDeleteModalCtrl', function($scope, $uibModalInstance, index, text) {
+
+  $scope.index = index;
+  $scope.text = text;
+
+  $scope.confirmDeleteRow = function () {
+    $uibModalInstance.close($scope.index);
+  };
+
+  $scope.closeModal = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+
+});
+
+// Modal for adding new print and copy related rows to some table in the price table
+app.controller('NewPrintCopyEntryCtrl', function($scope, $uibModalInstance, index, text) {
 
   $scope.index = index;
   $scope.text = text;
