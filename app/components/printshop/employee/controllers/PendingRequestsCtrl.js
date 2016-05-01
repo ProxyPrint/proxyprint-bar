83eaa9111f37731d10e0e64d73830c3f8f4ddff5
MarceloRGonc <marcelo.rgonc@gmail.com>
2016-05-01 12:36:25 +0100

    List print requests.

--- app/components/printshop/employee/controllers/PendingRequestsCtrl.js
+++ app/components/printshop/employee/controllers/PendingRequestsCtrl.js
@@ -1,22 +1,22 @@
 angular.module('ProxyPrint').controller('PendingRequestsCtrl', ['$scope', 'pendingPrintRequests', 'PendingPrintRequestsService',
-      function($scope, pendingPrintRequests, PendingPrintRequestsService) {
+function($scope, pendingPrintRequests, PendingPrintRequestsService) {
 
-   $scope.pendingRequests = [
-     { id: "1", consumer: "João Matias", arrivalTimestamp: "11/7/2014 12:15"},
-     { id: "2", consumer: "Costelo", arrivalTimestamp: "11/7/2014 11:15"},
-     { id: "3", consumer: "Maria Bina", arrivalTimestamp: "11/7/2014 10:22"},
-     { id: "4", consumer: "Joana Freitas", arrivalTimestamp: "11/7/2014 03:30"},
-     { id: "5", consumer: "Daniel Caldas", arrivalTimestamp: "11/7/2014 02:10"}
-   ];
-   // $scope.pendingRequests = pendingPrintRequests.data;
-   $scope.paginationOn = true;
+    // $scope.pendingRequests = [
+    //     { id: "1", consumer: "João Matias", arrivalTimestamp: "11/7/2014 12:15"},
+    //     { id: "2", consumer: "Costelo", arrivalTimestamp: "11/7/2014 11:15"},
+    //     { id: "3", consumer: "Maria Bina", arrivalTimestamp: "11/7/2014 10:22"},
+    //     { id: "4", consumer: "Joana Freitas", arrivalTimestamp: "11/7/2014 03:30"},
+    //     { id: "5", consumer: "Daniel Caldas", arrivalTimestamp: "11/7/2014 02:10"}
+    // ];
+    $scope.pendingRequests = angular.fromJson(pendingPrintRequests.data.printrequest);
+    $scope.paginationOn = true;
 
-   $scope.attendRequest = function(requestID) {
-     window.alert("Atender pedido "+requestID);
-   };
+    $scope.attendRequest = function(requestID) {
+        window.alert("Atender pedido "+requestID);
+    };
 
-   $scope.cancelRequest = function(requestID) {
-     window.alert("Cancelar pedido "+requestID);
-   };
+    $scope.cancelRequest = function(requestID) {
+        window.alert("Cancelar pedido "+requestID);
+    };
 
 }]);
