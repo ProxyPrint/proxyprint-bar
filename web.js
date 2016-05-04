48a0d219ed4c7a5035cbecc0182680cb2759928c
José Sousa <josesousa9000@gmail.com>
2016-05-04 19:06:51 +0100

    use port from ENV Variable

---
+++ web.js
@@ -0,0 +1,9 @@
+var port = Number(process.env.PORT || 9000);
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/'));
+var server = app.listen(port, function() {
+  console.log('Listening on port %d', server.address().port);
+});
