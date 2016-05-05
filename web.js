cc9240176399087673f9682f269986e58e1229f6
José Francisco Ferreira Alves de Sousa <josesousa9000@gmail.com>
2016-05-05 09:54:55 +0100

    clustering the node server + compression

--- web.js
+++ web.js
@@ -1,9 +1,21 @@
-var port = Number(process.env.PORT || 9000);
-
+var cluster = require('cluster');
+var compression = require('compression');
 var express = require('express');
 var app = express();
+app.use(compression());
 
-app.use(express.static(__dirname + '/'));
-var server = app.listen(port, function() {
-  console.log('Listening on port %d', server.address().port);
-});
+if (cluster.isMaster) {
+  var cpuCount = require('os').cpus().length;
+  console.log('running on %d cpus', cpuCount);
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
+
+} else {
+  var port = Number(process.env.PORT || 9000);
+
+  app.use(express.static(__dirname + '/'));
+  var server = app.listen(port, function() {
+    console.log('listening on port %d', server.address().port);
+  });
+}
