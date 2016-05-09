4c1095318470a51164ac108518081e662c0f4891
danielcaldas <danielcaldas@sapo.pt>
2016-05-09 12:10:16 +0100

    Delete employee OK.

--- app/components/printshop/manager/controllers/ManagerEmployeesCtrl.js
+++ app/components/printshop/manager/controllers/ManagerEmployeesCtrl.js
@@ -1,5 +1,5 @@
-angular.module('ProxyPrint').controller('ManagerEmployeesCtrl', ['$scope', '$state', 'employeesList',
-function($scope, $state, employeesList) {
+angular.module('ProxyPrint').controller('ManagerEmployeesCtrl', ['$scope', '$state', 'employeesList', 'employeesService', '$uibModal',
+function($scope, $state, employeesList, employeesService, $uibModal) {
 
   $scope.employees = employeesList.data.employees;
 
@@ -7,8 +7,62 @@ function($scope, $state, employeesList) {
     return (emp.name.match($scope.employeeSearch) || emp.username.match($scope.employeeSearch));
   };
 
+  // CRUD operations
+
+  // add
   $scope.newEmployee = function() {
     alert("new emp");
   };
 
+  // delete
+  $scope.confirmDelete = function(index) {
+    employeesService.setCurrentIndex(index);
+    $scope.openConfirmDeleteModal("Tem a certeza que pertende eliminar este empregado?");
+  };
+
+  $scope.openConfirmDeleteModal = function(reply) {
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: 'app/components/printshop/manager/views/employees/delete-employee-modal.html',
+      controller: 'ConfirmDeleteModalCtrl',
+      size: 'sm',
+      resolve: {
+        index: function() {
+          return $scope.index;
+        },
+        text: function() {
+          return reply;
+        }
+      }
+    });
+    modalInstance.result.then(function(index) {
+      index = employeesService.getCurrentIndex();
+      var res = employeesService.deleteEmployee($scope.employees[index].id);
+      if(res.success) {
+        $scope.employees.splice(index,1);
+        alert("Empregado removido com sucesso.");
+      } else {
+        alert("Foi impossível remover o empregado. Por favor tente mais tarde");
+      }
+    });
+  };
+
 }]);
+
+
+/*------------------
+ Modals
+------------------*/
+
+// Modal for deleting an employee
+app.controller('ConfirmDeleteModalCtrl', function($scope, $uibModalInstance, index, text) {
+  $scope.index = index;
+  $scope.text = text;
+  $scope.confirmDeleteRow = function () {
+    $uibModalInstance.close($scope.index);
+  };
+  $scope.closeModal = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+
+});
--- app/components/printshop/manager/services/EmployeesService.js
+++ app/components/printshop/manager/services/EmployeesService.js
@@ -2,13 +2,28 @@ var app = angular.module('ProxyPrint');
 
 app.factory('employeesService', ['$http', '$cookieStore','backendURLService',function($http, $cookieStore,backendURLService) {
   var service = {};
+  service.currentIndex = "";
 
   service.getEmployeesList = function() {
     var url = backendURLService.getBaseURL()+'printshops/'+$cookieStore.get("printShopID")+'/employees';
-    console.log("getEmployeesList");
     return $http.get(url).success(function(response){
       return response;
     });
+  };
+
+  service.deleteEmployee = function(employeeID) {
+    var url = backendURLService.getBaseURL()+'printshops/'+$cookieStore.get("printShopID")+'/employees/'+employeeID;
+    return $http.delete(url).success(function(response){
+      return response;
+    });
+  };
+
+  service.setCurrentIndex = function(index) {
+    service.currentIndex = index;
+  };
+
+  service.getCurrentIndex = function() {
+    return service.currentIndex;
   };
 
   return service;
