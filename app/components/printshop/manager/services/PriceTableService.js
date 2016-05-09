7327cd909e688698ed8c16d36d242317ed341230
danielcaldas <danielcaldas@sapo.pt>
2016-05-09 09:37:10 +0100

    Cover rows edition with back-end OK!

--- app/components/printshop/manager/services/PriceTableService.js
+++ app/components/printshop/manager/services/PriceTableService.js
@@ -75,10 +75,10 @@ app.factory('priceTableService', ['$http', '$cookieStore','backendURLService',fu
 
   service.editCoverRow = function(row) {
     service.newEntry = row;
-    /*var url = backendURLService.getBaseURL()+'printshops/'+$cookieStore.get("printShopID")+'/pricetable/covers';
+    var url = backendURLService.getBaseURL()+'printshops/'+$cookieStore.get("printShopID")+'/pricetable/covers';
     return $http.put(url,row).success(function(data){
       return data;
-    });*/
+    });
   };
 
   service.deleteCoverRow = function(row) {
