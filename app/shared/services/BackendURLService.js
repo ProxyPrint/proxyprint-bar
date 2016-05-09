c72a80360bc265142ad112639cbd4a94b1326401
José Sousa <josesousa9000@gmail.com>
2016-05-09 23:58:15 +0100

    get backend url from env variable

--- app/shared/services/BackendURLService.js
+++ app/shared/services/BackendURLService.js
@@ -3,7 +3,7 @@ angular.module('ProxyPrint').factory('backendURLService', [function() {
   var service = {};
 
   service.getBaseURL = function () {
-    return 'https://proxyprint-kitchen.herokuapp.com/';
+    return '/api/';
   };
 
   return service;
