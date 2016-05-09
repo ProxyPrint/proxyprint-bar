960c93407f3d7ffaf429bd697714fe1f582ffd01
José Sousa <josesousa9000@gmail.com>
2016-05-09 20:29:27 +0100

    use async queue para processar pdfs

--- app/components/consumer/services/FileTransferService.js
+++ app/components/consumer/services/FileTransferService.js
@@ -3,44 +3,54 @@ angular.module('ProxyPrint').factory('fileTransferService', ['Upload', '$timeout
     var service = {};
     service.processedFiles = {};
     service.processedFiles.printshops = printShopListService.getSelectedPrintShopsIDs();
-    service.processedFiles.files = [];
+    service.processedFiles.files = {};
+
+    var readPages = function(file, callback) {
+        var reader = new FileReader();
+        //callback quando filereader acaba
+        reader.onload = function(event) {
+            var contents = event.target.result;
+            PDFJS.getDocument(contents).then(function(doc) {
+                var numPages = doc.numPages;
+                console.log('# Document Loaded');
+                console.log('Number of Pages: ' + numPages);
+                callback({
+                    "name": file.name,
+                    "pages": numPages,
+                    "specs": file.specs
+                });
+            });
+        };
+        //callback quando filereader falha
+        reader.onerror = function(event) {
+            console.error("File could not be read! Code " + event.target.error.code);
+            callback();
+        };
+        //ler ficheiro
+        reader.readAsArrayBuffer(file);
+    }
 
     service.TransferFiles = function(files, callback) {
         var filesToStore = [];
+        var queue = async.queue(readPages, 1);
         for (var i = 0; i < files.length; i++) {
-            var tmp;
             var file = files[i];
-
             // Attention data is being lost here!
             filesToStore.push(file.name);
-
             if (!file.$error) {
-                var reader = new FileReader();
-
-                reader.onload = function(event) {
-                    var contents = event.target.result;
-                    PDFJS.getDocument(contents).then(function(doc) {
-                        var numPages = doc.numPages;
-                        console.log('# Document Loaded');
-                        console.log('Number of Pages: ' + numPages);
-                        service.processedFiles.files[file.name] = {
-                            specs: file.specs,
-                            pages: numPages
-                        }
-                        console.log(service.processedFiles);
-                    });
-                };
-
-                reader.onerror = function(event) {
-                    console.error("File could not be read! Code " + event.target.error.code);
-                };
-
-                reader.readAsArrayBuffer(file);
+                queue.push(files[i], function(processedFile) {
+                    console.log(processedFile);
+                    service.processedFiles.files[processedFile.name] = {
+                        specs: processedFile.specs,
+                        pages: processedFile.pages
+                    }
+                });
             }
         }
-
-        // Persist files in cookies
         $cookieStore.put("uploadedFilesNames", filesToStore);
+        queue.drain = function() {
+            console.log(service.processedFiles);
+        }
     };
 
     service.setFiles = function(files) {
