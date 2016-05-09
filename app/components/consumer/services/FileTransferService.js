0476683776aa7cfd5e914f78bfe36b9b7025e0f9
José Sousa <josesousa9000@gmail.com>
2016-05-09 18:20:13 +0100

    process files on the clientside

--- app/components/consumer/services/FileTransferService.js
+++ app/components/consumer/services/FileTransferService.js
@@ -1,50 +1,61 @@
-angular.module('ProxyPrint').factory('fileTransferService',['Upload','$timeout', 'backendURLService', '$cookieStore', function (Upload, $timeout, backendURLService, $cookieStore) {
+angular.module('ProxyPrint').factory('fileTransferService', ['Upload', '$timeout', 'backendURLService', '$cookieStore', function(Upload, $timeout, backendURLService, $cookieStore) {
 
-  var service = {};
-  service.TransferFiles = function (files, callback) {
-    var filesToStore = [];
-    for (var i = 0; i < files.length; i++) {
-      var tmp;
-      var file = files[i];
+    var service = {};
+    service.processedFiles = {};
 
-      // Attention data is being lost here!
-      filesToStore.push(file.name);
+    service.TransferFiles = function(files, callback) {
+        var filesToStore = [];
+        for (var i = 0; i < files.length; i++) {
+            var tmp;
+            var file = files[i];
 
-      if (!file.$error) {
-        Upload.upload({
-          url: backendURLService.getBaseURL()+'consumer/upload',
-          data: {
-            file: file,
-            spec: files[i].specs
-          }
-        }).then(function (resp) {
-          $timeout(function() {
-            console.log('file: '+resp.config.data.file.name);
-            console.log('Response: ' + JSON.stringify(resp.data));
-          });
-        }, null, function (evt) {
-          var progressPercentage = parseInt(100.0 *
-            evt.loaded / evt.total);
-            console.log('progress: ' + progressPercentage +'% ' + evt.config.data.file.name + '\n');
-          });
+            // Attention data is being lost here!
+            filesToStore.push(file.name);
+
+            if (!file.$error) {
+                var reader = new FileReader();
+
+                reader.onload = function(event) {
+                    var contents = event.target.result;
+                    PDFJS.getDocument(contents).then(function(doc) {
+                        var numPages = doc.numPages;
+                        console.log('# Document Loaded');
+                        console.log('Number of Pages: ' + numPages);
+                        service.processedFiles[file.name] = {
+                          specs: file.specs,
+                          pages: numPages
+                        }
+                        console.log(service.processedFiles);
+                    });
+                };
+
+                reader.onerror = function(event) {
+                    console.error("File could not be read! Code " + event.target.error.code);
+                };
+
+                reader.readAsArrayBuffer(file);
+            }
         }
-      }
 
-      // Persist files in cookies
-      $cookieStore.put("uploadedFilesNames", filesToStore);
+        // Persist files in cookies
+        $cookieStore.put("uploadedFilesNames", filesToStore);
     };
 
-    service.setFiles = function (files){
-      angular.forEach(files, function(file){
-        file.specs = [];
-      });
+    service.setFiles = function(files) {
+        angular.forEach(files, function(file) {
+            file.specs = [];
+        });
 
-      service.files = files;
+        service.files = files;
     };
 
-    service.getFiles = function () {
-      return service.files;
+    service.getFiles = function() {
+        return service.files;
     };
+
+    service.getProcessedFiles = function() {
+      return service.processedFiles;
+    }
 
     return service;
-  }]);
+}]);
