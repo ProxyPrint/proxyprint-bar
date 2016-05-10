06b7155472d017937c36dc88bef3f43b9fcdd4db
José Sousa <josesousa9000@gmail.com>
2016-05-10 01:58:49 +0100

    temporiaramente utilizar hardcoded url

--- app/shared/services/BackendURLService.js
+++ app/shared/services/BackendURLService.js
@@ -2,8 +2,8 @@ angular.module('ProxyPrint').factory('backendURLService', [function() {
 
   var service = {};
 
-  service.getBaseURL = function () {
-    return '/api/';
+  service.getBaseURL = function() {
+    return'https://proxyprint-kitchen.herokuapp.com/';
   };
 
   return service;
