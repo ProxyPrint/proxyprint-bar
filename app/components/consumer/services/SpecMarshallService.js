963b90b2d48fec4d5ee7ceb66ec7203b18b7787f
danielcaldas <danielcaldas@sapo.pt>
2016-05-16 00:37:07 +0100

    Temporary fix for SpecMarshallService.js.

--- app/components/consumer/services/SpecMarshallService.js
+++ app/components/consumer/services/SpecMarshallService.js
@@ -1,4 +1,4 @@
-angular.module('ProxyPrint').factory('specMarshallService',[function () {
+/*angular.module('ProxyPrint').factory('specMarshallService',[function () {
 
     var service = {};
 
@@ -74,6 +74,39 @@ angular.module('ProxyPrint').factory('specMarshallService',[function () {
 
 
 
+
+
+
+    return service;
+}]);*/
+angular.module('ProxyPrint').factory('specMarshallService',[function () {
+
+    var service = {};
+
+    service.marshallSpecification = function (specification){
+      var spec = new Object();
+
+      spec.name = specification[0];
+      // PAPER,COLOR,A4,DUPLEX --> correct key order
+      spec.paperSpecs = "PAPER,"+(specification[3])+","+specification[1]+","+specification[2];
+
+        if (specification[4]==null){
+          spec.bindingSpecs = spec.coverSpecs = "";
+        }
+
+        else {
+          if (specification[5]==null && specification[6]==null){
+            spec.bindingSpecs = "STAPLING";
+            spec.coverSpecs = "";
+          }
+          else {
+            spec.coverSpecs = "COVER,"+specification[5];
+            spec.bindingSpecs = "BINDING,"+specification[6]+","+specification[1];
+          }
+        }
+        return spec;
+
+    }
 
 
 
