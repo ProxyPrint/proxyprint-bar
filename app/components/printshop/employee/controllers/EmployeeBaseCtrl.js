f89995e7adab20a8c5b2781a768132905bfc17ce
MarceloRGonc <marcelo.rgonc@gmail.com>
2016-05-15 16:33:42 +0100

    Correct some things.

--- app/components/printshop/employee/controllers/EmployeeBaseCtrl.js
+++ app/components/printshop/employee/controllers/EmployeeBaseCtrl.js
@@ -1,18 +1,18 @@
 angular.module('ProxyPrint').controller('EmployeeBaseCtrl', ['$scope', '$cookieStore', 'authenticationService', '$state',
-      function($scope, $cookieStore, authenticationService, $state) {
+function($scope, $cookieStore, authenticationService, $state) {
 
-   $scope.employee = $cookieStore.get('globals').currentUser;
+    $scope.employee = $cookieStore.get('globals').currentUser;
 
-   $scope.logout = function(){
-      authenticationService.ClearCredentials();
-      $state.go('printshop');
-   };
+    $scope.logout = function(){
+        authenticationService.ClearCredentials();
+        $state.go('printshop');
+    };
 
-   // Navigation highlight
-   $scope.navigation = { pending: "active", satisfied: "", history: "" };
-   $scope.navigate = function(where) {
-     for(var i in $scope.navigation){ $scope.navigation[i] = ""; }
-     $scope.navigation[where] = "active";
-   };
+    // Navigation highlight
+    $scope.navigation = { pending: "active", satisfied: "", history: "" };
+    $scope.navigate = function(where) {
+        for(var i in $scope.navigation){ $scope.navigation[i] = ""; }
+        $scope.navigation[where] = "active";
+    };
 
 }]);
