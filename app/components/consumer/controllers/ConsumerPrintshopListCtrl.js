329ea0b45f7494bc6f37b20158fe42f3d6a0ab32
Cortezz <jospcortez@gmail.com>
2016-05-18 13:54:16 +0100

    State transition from printshop list to a specific printshop.

--- app/components/consumer/controllers/ConsumerPrintshopListCtrl.js
+++ app/components/consumer/controllers/ConsumerPrintshopListCtrl.js
@@ -1,19 +1,26 @@
 angular.module('ProxyPrint')
-   .controller('ConsumerPrintshopList', ['$scope' , function ($scope) {
+   .controller('ConsumerPrintshopList', ['$scope','$state', function ($scope,$state) {
 
       $scope.printshops = [{
+            id: 1,
             name: "Impressões Jerónimo",
             address: "Avenida Dr. Filipe Veríssimo, 19 Lourimé",
             rating: 3
          }, {
+            id: 2,
             name: "Copy & Paste",
             address: "Rua Augusto Semedo, 1086 Barrada",
             rating: 4
          },{
+            id: 3,
             name: "Print4U",
             address: "Alameda Ana Fernandes, Porto",
             rating: 5
          }
       ];
+
+      $scope.loadPrintshop = function (index) {
+        $state.go('printshoppage', {printshopid: $scope.printshops[index].id});
+      }
 
 }]);
