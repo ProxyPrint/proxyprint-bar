10ced4433402075059adc255842b9510ee12b1be
danielcaldas <danielcaldas@sapo.pt>
2016-05-19 18:07:17 +0100

    Added paypal payment operation in print request flow. Looking good.

--- app/components/consumer/controllers/ConsumerBudgetSelectionCtrl.js
+++ app/components/consumer/controllers/ConsumerBudgetSelectionCtrl.js
@@ -7,10 +7,14 @@ function($scope, $cookieStore, $state, budgets, printShopListService, fileTransf
   $scope.submitedFiles = fileTransferService.getProcessedFiles().files;
   $scope.submitedFilesNames = Object.keys($scope.submitedFiles);
 
+  $scope.amount = 0.0;
+  $scope.blockBudget = false;
+
   for(var pshopID in $scope.budgets) {
     if(!isNaN($scope.budgets[pshopID])) {
       $scope.selectedPrintShops[pshopID]['hasBudget'] = true;
       $scope.selectedPrintShops[pshopID]['budget'] = parseFloat($scope.budgets[pshopID]).toFixed(2);
+      $scope.blockBudget = true;
     } else {
       $scope.selectedPrintShops[pshopID]['budget'] = $scope.budgets[pshopID];
     }
@@ -23,7 +27,9 @@ function($scope, $cookieStore, $state, budgets, printShopListService, fileTransf
     console.log($scope.theChosenOne);
     if($scope.theChosenOne!==null && $scope.theChosenOne > 0) {
       var submitParams = {printshopID: $scope.theChosenOne , budget: parseFloat($scope.budgets[$scope.theChosenOne])};
-      budgetService.submitPrintRequest($scope.submitRequestSuccessCallback, $scope.submitRequestErrorCallback, $scope.printRequestID, submitParams);
+      // Set amount for pay pal payment
+      $scope.amount = parseFloat($scope.budgets[$scope.theChosenOne]).toFixed(2);
+      // budgetService.submitPrintRequest($scope.submitRequestSuccessCallback, $scope.submitRequestErrorCallback, $scope.printRequestID, submitParams);
     } else {
       alert("Por favor escolha de entre uma das reprografias. Caso nenhuma satisfaça o pedido volte atrás e tente outras reprografias.");
     }
