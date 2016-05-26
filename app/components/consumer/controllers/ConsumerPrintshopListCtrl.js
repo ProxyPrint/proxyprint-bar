3eacfc6daa0c22464edb3bb27e7a59eea25858c2
Cortezz <jospcortez@gmail.com>
2016-05-26 16:49:31 +0100

    Couple of fixes on the infinite scroll directive.

--- app/components/consumer/controllers/ConsumerPrintshopListCtrl.js
+++ app/components/consumer/controllers/ConsumerPrintshopListCtrl.js
@@ -3,14 +3,18 @@ angular.module('ProxyPrint')
         function ($scope,$state,printshops) {
 
       $scope.printshops = printshops.data;
-      $scope.limit = 5;
+      $scope.limit = 6;
       $scope.loadPrintshop = function (index) {
         $state.go('consumer.printshop', {printshopid: index});
       }
 
+
+
       $scope.loadMore = function() {
-            var incremented = $scope.limit + 5;
+            var incremented = $scope.limit + 6;
             $scope.limit = incremented > $scope.printshops.length ? $scope.printshops.length : incremented;
+
         };
+
 
 }]);
