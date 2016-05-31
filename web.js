557ad258f47e5db43ebb7d58b25bb8a4612b6df7
danielcaldas <danielcaldas@sapo.pt>
2016-05-31 22:22:48 +0100

    Dynamic external URL on web.js generated by ngrok.

--- web.js
+++ web.js
@@ -4,17 +4,30 @@ var express = require('express');
 var minify = require('express-minify');
 var redirect = require("express-redirect");
 var app = express();
-var backendURL = process.env.BACKEND_URL || "http://localhost:8080/"
-var port = Number(process.env.PORT || 9000);
 
-if (cluster.isMaster) {
+var ngrok = require('ngrok');
+var externalURL="";
+var port = 8080;
+ngrok.once('connect', function (url) {
+  externalURL=url+"/";
+
+  // Write external URL to file
+  var fs = require('fs');
+  fs.writeFile("/tmp/externalURL", externalURL, function(err) {
+    if(err) { return console.log(err); }
+  });
+
+  var backendURL = process.env.BACKEND_URL || externalURL;
+  var port = Number(process.env.PORT || 9000);
+
+  if (cluster.isMaster) {
     var cpuCount = require('os').cpus().length;
     console.log('running on %d cpus, backendURL: %s', cpuCount, backendURL);
     for (var i = 0; i < cpuCount; i += 1) {
-        cluster.fork();
+      cluster.fork();
     }
 
-} else {
+  } else {
     app.use(compression());
     app.use(minify());
     redirect(app);
@@ -22,10 +35,13 @@ if (cluster.isMaster) {
     app.use(express.static(__dirname + '/'));
 
     app.get("/config", function(req, res) {
-        res.send(backendURL);
+      res.send(backendURL);
     });
 
     var server = app.listen(port, function() {
-        console.log('listening on port %d', server.address().port);
+      console.log('listening on port %d', server.address().port);
     });
-}
+  }
+
+});
+ngrok.connect(port);
