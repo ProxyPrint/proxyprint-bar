e1d16f1730046b56c098b76b5054a2a12cffe147
danielcaldas <danielcaldas@sapo.pt>
2016-06-03 02:38:46 +0100

    Add payment method selection modal. Moved spin.min.js to assets/libs/.

--- app/components/consumer/controllers/ConsumerBudgetSelectionCtrl.js
+++ app/components/consumer/controllers/ConsumerBudgetSelectionCtrl.js
@@ -1,5 +1,5 @@
-angular.module('ProxyPrint').controller('ConsumerBudgetSelectionCtrl', ['$scope','$cookieStore', '$state', 'budgets', 'printShopListService', 'fileTransferService', 'budgetService', 'backendURLService',
-function($scope, $cookieStore, $state, budgets, printShopListService, fileTransferService, budgetService, backendURLService) {
+angular.module('ProxyPrint').controller('ConsumerBudgetSelectionCtrl', ['$scope','$cookieStore', '$state', 'budgets', 'printShopListService', 'fileTransferService', 'budgetService', 'backendURLService', '$uibModal',
+function($scope, $cookieStore, $state, budgets, printShopListService, fileTransferService, budgetService, backendURLService, $uibModal) {
 
   $scope.budgets = budgets.budgets;
   console.log($scope.budgets);
@@ -58,7 +58,52 @@ function($scope, $cookieStore, $state, budgets, printShopListService, fileTransf
   };
 
   $scope.goBackToConsumerLandingPage = function() {
+    // Open modal for choosing payment method
     $state.go('consumer.mainpage');
+  };
+
+  $scope.pay = function() {
+    // ... pay
+    $scope.openPaymentMethodSelectionModal("Escolha o método de pagamento", $scope.amount, $scope.payPalCallbackUrl);
+  };
+
+  $scope.openPaymentMethodSelectionModal = function(text,amount,callbackURL) {
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: 'app/components/consumer/views/consumer-payment-method-selection.html',
+      controller: 'PaymentMethodSelectionCtrl',
+      size: 'md',
+      resolve: {
+        text: function() {
+          return text;
+        },
+        amount: function() {
+          return amount;
+        },
+        callbackURL: function() {
+          return callbackURL;
+        }
+      }
+    });
+    modalInstance.result.then(function(index) {
+      //...
+    });
+  };
+
+}]);
+
+app.controller('PaymentMethodSelectionCtrl', ['$scope', '$uibModalInstance', 'text', 'amount', 'callbackURL', function($scope, $uibModalInstance, text, amount, callbackURL) {
+
+  $scope.text = text;
+  $scope.amount = amount;
+  $scope.payPalCallbackUrl = callbackURL;
+
+  $scope.addNewEntry = function () {
+    //...
+  };
+
+  $scope.closeModal = function () {
+    //...
   };
 
 }]);
