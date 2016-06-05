2e2dc891f8703ea32fc7229f0e297410d6674ace
Cortezz <jospcortez@gmail.com>
2016-06-05 20:24:57 +0100

    Selenium server setup. First protractor tests

---
+++ tests/e2e/basictest.spec.js
@@ -0,0 +1,7 @@
+describe('angularjs homepage', function() {
+  it('should have a title', function() {
+    browser.get('http://localhost:9000');
+
+    expect(browser.getTitle()).toContain('ProxyPrint');
+  });
+});
