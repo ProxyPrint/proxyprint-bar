1aac0abb90d33b54901695c65bc191532c06cbab
Cortezz <jospcortez@gmail.com>
2016-06-06 16:10:54 +0100

    Printshop page tests

--- tests/e2e/consumer_mainpage.spec.js
+++ tests/e2e/consumer_mainpage.spec.js
@@ -11,20 +11,20 @@ describe('consumer mainpage tests', function() {
   });
 
     it('Printshop list page should be working', function() {
-      element(by.css('.fa-book')).click().then( function () {
+      $('.fa-book').click().then( function () {
         expect(browser.getCurrentUrl()).toEqual(user_url+'/printshops');
       });
     });
 
     it('Request history page should be working', function() {
-      element(by.css('.fa-history')).click().then( function () {
+      $('.fa-history').click().then( function () {
         expect(browser.getCurrentUrl()).toEqual(user_url+'/history');
       });
     });
 
     it('Settings page should be working', function() {
-      element(by.css('.caret')).click();
-      element(by.css('.fa-cog')).click().then(function () {
+      $('.caret').click();
+      $('.fa-cog').click().then(function () {
         expect(browser.getCurrentUrl()).toEqual(user_url+'/settings')
       })
     });
--- tests/e2e/printshoplist.spec.js
+++ tests/e2e/printshoplist.spec.js
@@ -8,17 +8,33 @@ describe('consumer printshop list tests', function() {
     element(by.model('username')).sendKeys("joao");
     element(by.model('password')).sendKeys("1234");
     element(by.id('loginButton')).click();
-    element(by.css('.fa-book')).click();
+    $('.fa-book').click();
   });
 
 
 
-    it('CopyScan should be found', function() {
-      //var printshopName = element(by.repeater('printshop in printshops').row(0));
+    it('Clicking in a printshop should open its page', function() {
+      var printshopName = element(by.repeater('printshop in printshops').row(0)).element(by.tagName('strong')).getText();
       element(by.repeater('printshop in printshops').row(0)).click().then(function () {
         var name = element(by.binding('printshop.name')).getText();
-        expect(name).toBe("Copy Scan");
+        expect(name).toBe(printshopName);
       });
+    });
+
+    it('Adding a review should increase the amount of reviews', function () {
+      element(by.repeater('printshop in printshops').row(0)).click();
+      browser.executeScript('window.scrollTo(0,100000);').then(function () {
+        element.all(by.repeater('review in printshop.reviews | limitTo: limit')).count().then(function (count){
+          $('.fa-star').click();
+          element(by.model('content')).sendKeys("Mau serviço!");
+          $('.modal-footer').$('.button-primary').click().then(function () {
+            browser.executeScript('window.scrollTo(0,100000);').then(function () {
+              expect(element.all(by.repeater('review in printshop.reviews')).count()).toEqual(count+1);
+            });
+          });
+        });
+      });
+
     });
 
 
