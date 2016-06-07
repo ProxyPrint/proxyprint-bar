08625f1b0c09fbb6e52f6050f79016d44cead0fa
José Sousa <josesousa9000@gmail.com>
2016-06-07 22:29:44 +0100

    if case for no tunneling (paypal)

--- app/components/consumer/controllers/ConsumerBudgetSelectionCtrl.js
+++ app/components/consumer/controllers/ConsumerBudgetSelectionCtrl.js
@@ -1,82 +1,95 @@
-angular.module('ProxyPrint').controller('ConsumerBudgetSelectionCtrl', ['$scope','$cookieStore', '$state', 'budgets', 'printShopListService', 'fileTransferService', 'budgetService', 'backendURLService', 'usSpinnerService', '$uibModal',
-function($scope, $cookieStore, $state, budgets, printShopListService, fileTransferService, budgetService, backendURLService, usSpinnerService, $uibModal) {
+angular.module('ProxyPrint').controller('ConsumerBudgetSelectionCtrl', ['$scope', '$cookieStore', '$state', 'budgets', 'printShopListService', 'fileTransferService', 'budgetService', 'backendURLService', 'usSpinnerService', '$uibModal',
+  function($scope, $cookieStore, $state, budgets, printShopListService, fileTransferService, budgetService, backendURLService, usSpinnerService, $uibModal) {
 
-  $scope.budgets = budgets.budgets;
-  console.log($scope.budgets);
-  $scope.printRequestID = budgets.printRequestID;
-  $scope.selectedPrintShops = printShopListService.getSelectedPrintShops();
-  $scope.submitedFiles = fileTransferService.getProcessedFiles().files;
-  $scope.submitedFilesNames = Object.keys($scope.submitedFiles);
+    $scope.budgets = budgets.budgets;
+    console.log($scope.budgets);
+    $scope.printRequestID = budgets.printRequestID;
+    $scope.selectedPrintShops = printShopListService.getSelectedPrintShops();
+    $scope.submitedFiles = fileTransferService.getProcessedFiles().files;
+    $scope.submitedFilesNames = Object.keys($scope.submitedFiles);
 
-  $scope.amount = 0.0;
-  $scope.selectedPrintShopName = "";
-  if(backendURLService.getBaseURL().match("localhost")) {
-    $scope.payPalCallbackUrl = budgets.externalURL+"paypal/ipn/";
-  } else {
-    $scope.payPalCallbackUrl = budgets.externalURL+backendURLService.getContextPath()+"paypal/ipn/";
-  }
-  console.log($scope.payPalCallbackUrl);
-
-  for(var pshopID in $scope.budgets) {
-    if(!isNaN($scope.budgets[pshopID])) {
-      $scope.selectedPrintShops[pshopID].hasBudget = true;
-      $scope.selectedPrintShops[pshopID].budget = parseFloat($scope.budgets[pshopID]).toFixed(2);
-    } else {
-      $scope.selectedPrintShops[pshopID].budget = $scope.budgets[pshopID];
-    }
-  }
-  usSpinnerService.stop('consumer-spinner');
-  $scope.isSomePShopSelected = false;
-
-  $scope.finishPrintRequest = function() {
-    console.log($scope.printRequestID);
-    console.log($scope.theChosenOne);
-    if($scope.theChosenOne!==null && $scope.theChosenOne > 0) {
-      // Set amount for pay pal payment
-      $scope.amount = parseFloat($scope.budgets[$scope.theChosenOne]).toFixed(2);
-      // Finish the PayPal callback URL appending the printRequestID
-      $scope.payPalCallbackUrl += $scope.printRequestID;
-      $scope.selectedPrintShopName = $scope.selectedPrintShops[$scope.theChosenOne].name;
-      console.log($scope.payPalCallbackUrl);
-
-      $scope.submitParams = {printRequestID: $scope.printRequestID, printshopID: $scope.theChosenOne , budget: parseFloat($scope.budgets[$scope.theChosenOne]), paymentMethod: ""};
-    } else {
-      alert("Por favor escolha de entre uma das reprografias. Caso nenhuma satisfaça o pedido volte atrás e tente outras reprografias.");
-    }
-  };
-
-  $scope.pay = function() {
-    usSpinnerService.spin('consumer-spinner');
-    $scope.openPaymentMethodSelectionModal("Escolha o método de pagamento", $scope.amount, $scope.payPalCallbackUrl, $scope.selectedPrintShopName, $scope.submitParams);
-  };
-
-  $scope.openPaymentMethodSelectionModal = function(text, amount, callbackURL, pshopName, submitParams) {
-    var modalInstance = $uibModal.open({
-      animation: true,
-      templateUrl: 'app/components/consumer/views/consumer-payment-method-selection.html',
-      controller: 'PaymentMethodSelectionCtrl',
-      size: 'sm',
-      resolve: {
-        text: function() {
-          return text;
-        },
-        amount: function() {
-          return amount;
-        },
-        callbackURL: function() {
-          return callbackURL;
-        },
-        pshopName: function() {
-          return pshopName;
-        },
-        submitParams: function() {
-          return submitParams;
-        }
+    $scope.amount = 0.0;
+    $scope.selectedPrintShopName = "";
+    //caldas muda esta cena para o teu gosto
+    //basicamente se o externalURL tiver vazio quer dizer que nao ha tunel e
+    //deve se aceder direto
+    if (budgets.externalURL) {
+      if (backendURLService.getBaseURL().match("localhost")) {
+        $scope.payPalCallbackUrl = budgets.externalURL + "paypal/ipn/";
+      } else {
+        $scope.payPalCallbackUrl = budgets.externalURL + backendURLService.getContextPath() + "paypal/ipn/";
       }
-    });
-  };
+    } else {
+      $scope.payPalCallbackUrl = backendURLService.getBaseURL() + "paypal/ipn/";
+    }
+    console.log($scope.payPalCallbackUrl);
 
-}]);
+    for (var pshopID in $scope.budgets) {
+      if (!isNaN($scope.budgets[pshopID])) {
+        $scope.selectedPrintShops[pshopID].hasBudget = true;
+        $scope.selectedPrintShops[pshopID].budget = parseFloat($scope.budgets[pshopID]).toFixed(2);
+      } else {
+        $scope.selectedPrintShops[pshopID].budget = $scope.budgets[pshopID];
+      }
+    }
+    usSpinnerService.stop('consumer-spinner');
+    $scope.isSomePShopSelected = false;
+
+    $scope.finishPrintRequest = function() {
+      console.log($scope.printRequestID);
+      console.log($scope.theChosenOne);
+      if ($scope.theChosenOne !== null && $scope.theChosenOne > 0) {
+        // Set amount for pay pal payment
+        $scope.amount = parseFloat($scope.budgets[$scope.theChosenOne]).toFixed(2);
+        // Finish the PayPal callback URL appending the printRequestID
+        $scope.payPalCallbackUrl += $scope.printRequestID;
+        $scope.selectedPrintShopName = $scope.selectedPrintShops[$scope.theChosenOne].name;
+        console.log($scope.payPalCallbackUrl);
+
+        $scope.submitParams = {
+          printRequestID: $scope.printRequestID,
+          printshopID: $scope.theChosenOne,
+          budget: parseFloat($scope.budgets[$scope.theChosenOne]),
+          paymentMethod: ""
+        };
+      } else {
+        alert("Por favor escolha de entre uma das reprografias. Caso nenhuma satisfaça o pedido volte atrás e tente outras reprografias.");
+      }
+    };
+
+    $scope.pay = function() {
+      usSpinnerService.spin('consumer-spinner');
+      $scope.openPaymentMethodSelectionModal("Escolha o método de pagamento", $scope.amount, $scope.payPalCallbackUrl, $scope.selectedPrintShopName, $scope.submitParams);
+    };
+
+    $scope.openPaymentMethodSelectionModal = function(text, amount, callbackURL, pshopName, submitParams) {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'app/components/consumer/views/consumer-payment-method-selection.html',
+        controller: 'PaymentMethodSelectionCtrl',
+        size: 'sm',
+        resolve: {
+          text: function() {
+            return text;
+          },
+          amount: function() {
+            return amount;
+          },
+          callbackURL: function() {
+            return callbackURL;
+          },
+          pshopName: function() {
+            return pshopName;
+          },
+          submitParams: function() {
+            return submitParams;
+          }
+        }
+      });
+    };
+
+  }
+]);
 
 app.controller('PaymentMethodSelectionCtrl', ['$scope', '$state', 'toasterService', '$uibModalInstance', 'text', 'amount', 'callbackURL', 'pshopName', 'submitParams', 'budgetService', 'usSpinnerService', '$cookieStore', function($scope, $state, toasterService, $uibModalInstance, text, amount, callbackURL, pshopName, submitParams, budgetService, usSpinnerService, $cookieStore) {
 
@@ -106,75 +119,83 @@ app.controller('PaymentMethodSelectionCtrl', ['$scope', '$state', 'toasterServic
   };
 
   $scope.confirmPayment = function() {
-    if($scope.submitParams.paymentMethod==="PROXYPRINT_PAYMENT") {
+    if ($scope.submitParams.paymentMethod === "PROXYPRINT_PAYMENT") {
       $scope.payViaProxyPrint();
     } else {
       $scope.payViaProxyPrint();
     }
   };
 
-  $scope.payViaProxyPrint = function () {
+  $scope.payViaProxyPrint = function() {
     usSpinnerService.spin('consumer-spinner');
     budgetService.submitPrintRequest($scope.submitParams.printRequestID, $scope.submitParams).success(function(data) {
-      console.log(data);
-      if(data.success===true) {
-        var newAmount = subtractMoney($cookieStore.get("consumerBalance"), $scope.amount);
-        $cookieStore.put("consumerBalance", newAmount);
-        console.log(newAmount);
-        toasterService.notifySuccess("Pedido submetido com sucesso. Iremos notificá-lo assim que tudo estiver pronto. Obrigado!");
-      } else {
-        toasterService.notifyWarning("Infelizmente não possuí saldo suficiente para pagar o pedido.");
-      }
-      $state.go('consumer.mainpage', {}, { reload: true });
-      $uibModalInstance.dismiss('cancel');
-      usSpinnerService.stop('consumer-spinner');
-    })
-    .error(function(data) {
-      console.log(data);
-      toasterService.notifyError("Pedimos desculpa mas foi impossível de processar o seu pedido. Por favor tente mais tarde.");
-      $state.go('consumer.mainpage', {}, { reload: true });
-      $uibModalInstance.dismiss('cancel');
-      usSpinnerService.stop('consumer-spinner');
-    });
+        console.log(data);
+        if (data.success === true) {
+          var newAmount = subtractMoney($cookieStore.get("consumerBalance"), $scope.amount);
+          $cookieStore.put("consumerBalance", newAmount);
+          console.log(newAmount);
+          toasterService.notifySuccess("Pedido submetido com sucesso. Iremos notificá-lo assim que tudo estiver pronto. Obrigado!");
+        } else {
+          toasterService.notifyWarning("Infelizmente não possuí saldo suficiente para pagar o pedido.");
+        }
+        $state.go('consumer.mainpage', {}, {
+          reload: true
+        });
+        $uibModalInstance.dismiss('cancel');
+        usSpinnerService.stop('consumer-spinner');
+      })
+      .error(function(data) {
+        console.log(data);
+        toasterService.notifyError("Pedimos desculpa mas foi impossível de processar o seu pedido. Por favor tente mais tarde.");
+        $state.go('consumer.mainpage', {}, {
+          reload: true
+        });
+        $uibModalInstance.dismiss('cancel');
+        usSpinnerService.stop('consumer-spinner');
+      });
   };
 
-  $scope.payViaPayPal = function () {
+  $scope.payViaPayPal = function() {
     usSpinnerService.spin('consumer-spinner');
     budgetService.submitPrintRequest($scope.submitParams.printRequestID, $scope.submitParams).success(function(data) {
-      if(data.success===true) {
-        toasterService.notifySuccess("Assim que efetuar o pagamento no PayPal iremos registar o seu pedido. Obrigado!");
-      } else {
-        toasterService.notifyWarning("Infelizmente não conseguimos processar o seu pedido.");
-      }
-      $state.go('consumer.mainpage', {}, { reload: true });
-      $uibModalInstance.dismiss('cancel');
-      usSpinnerService.stop('consumer-spinner');
-    })
-    .error(function(data) {
-      toasterService.notifyError("Pedimos desculpa mas foi impossível de processar o seu pedido. Por favor tente mais tarde.");
-      $state.go('consumer.mainpage', {}, { reload: true });
-      $uibModalInstance.dismiss('cancel');
-      usSpinnerService.stop('consumer-spinner');
-    });
+        if (data.success === true) {
+          toasterService.notifySuccess("Assim que efetuar o pagamento no PayPal iremos registar o seu pedido. Obrigado!");
+        } else {
+          toasterService.notifyWarning("Infelizmente não conseguimos processar o seu pedido.");
+        }
+        $state.go('consumer.mainpage', {}, {
+          reload: true
+        });
+        $uibModalInstance.dismiss('cancel');
+        usSpinnerService.stop('consumer-spinner');
+      })
+      .error(function(data) {
+        toasterService.notifyError("Pedimos desculpa mas foi impossível de processar o seu pedido. Por favor tente mais tarde.");
+        $state.go('consumer.mainpage', {}, {
+          reload: true
+        });
+        $uibModalInstance.dismiss('cancel');
+        usSpinnerService.stop('consumer-spinner');
+      });
   };
 
-  function subtractMoney(balance,amount) {
+  function subtractMoney(balance, amount) {
     amountParts = amount.split(".");
     ip = parseInt(amountParts[0]);
     fp = parseInt(amountParts[1]);
     balance.integerPart -= ip;
     tmp = balance.fractionalPart - fp;
 
-    if(tmp <= 0) {
-        while(tmp <= 0) {
-            balance.integerPart--;
-            balance.fractionalPart = tmp + 100;
-            tmp += 100;
-        }
+    if (tmp <= 0) {
+      while (tmp <= 0) {
+        balance.integerPart--;
+        balance.fractionalPart = tmp + 100;
+        tmp += 100;
+      }
     } else {
-        balance.fractionalPart = tmp;
+      balance.fractionalPart = tmp;
     }
     return balance;
-}
+  }
 
 }]);
