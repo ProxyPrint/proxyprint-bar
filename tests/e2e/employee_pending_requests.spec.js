52f935693a372ccc64c727f566ec23212e65674f
Cortezz <jospcortez@gmail.com>
2016-06-12 16:17:25 +0100

    Improved Being Processed test

--- tests/e2e/employee_pending_requests.spec.js
+++ tests/e2e/employee_pending_requests.spec.js
@@ -3,6 +3,7 @@ describe('pending requests tests', function() {
   var base_url = 'http://localhost:9000';
   var user_url = base_url + '/#/employee/mafalda';
   var pendingRequestId = "4";
+  var beingProcessedId = "4";
 
   beforeEach(function () {
     browser.get(base_url);
@@ -12,15 +13,7 @@ describe('pending requests tests', function() {
   });
 
   it('Changing the status of a pending request should be propagated into the pending requests list', function () {
-    /*var index = 4;
-    requests = element.all(by.repeater('request in pendingRequests'));
-    requests.get(index).element($('.btn-primary ')).click();
-    expect(browser.getCurrentUrl()).toBe(user_url+'/pending/'+pendingRequestId);
-    expect($('.btn-success').isDisplayed()).toBe(false);
-    $('.fa-thumbs-o-up').click();
-    browser.get(user_url);
-    expect(element(by.repeater('request in pendingRequests').row(index).column(''))).toBe('A ser atendido');*/
-    var index = 2;
+    /*var index = 2;
     requests = element.all(by.repeater('request in pendingRequests'));
     requests.get(index).element(by.css('.btn-primary ')).click();
     expect(browser.getCurrentUrl()).toBe(user_url+'/pending/'+pendingRequestId);
@@ -28,7 +21,23 @@ describe('pending requests tests', function() {
     $('.fa-thumbs-o-up').click();
     $('[ng-click="performAction()"]').click();
     browser.get(user_url+'/pending/'+pendingRequestId);
-    expect($('.btn-success').isDisplayed()).toBe(true);
+    expect($('.btn-success').isDisplayed()).toBe(true);*/
+  });
+
+  it('Changing the status of a "being processed" Request should be propagated into the every other list', function () {
+    var index = 0;
+    requests = element.all(by.repeater('request in pendingRequests'));
+    requests.get(index).element(by.css('.btn-primary')).click();
+    expect(browser.getCurrentUrl()).toBe(user_url+'/pending/'+beingProcessedId);
+    expect($('.btn-success').isDisplayed()).toBe(true)
+    $('.btn-success').click();
+    $('[ng-click="performAction()"]').click().then(function () {
+      $('[ng-click="performAction()"]').click().then(function () {
+        expect(element(by.binding('message'))).toBe('Finalizado');
+        //expect($('.btn-success').isDisplayed()).toBe(false);
+        //browser.get(user_url+'/pending/'+beingProcessedId);
+      })
+    });
   });
 
 });
